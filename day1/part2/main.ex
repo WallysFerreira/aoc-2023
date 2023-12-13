8d5ac97de111f0ba3947a84e49cc6b1283079fc5
Wallys Ferreira <wallysfsilva@gmail.com>
2023-12-13 10:59:56 -0300

    leave first and last letter of replaced number

--- day1/part2/main.ex
+++ day1/part2/main.ex
@@ -1,11 +1,19 @@
-# Need to normalize string in the order that the number appears on the string
-
 defmodule Solve do
     def normalize_string(string) do
-        names_numbers = %{"one" => "1", "two" => "2"}
+        names_numbers = %{
+            "one" => "1",
+            "two" => "2",
+            "three" => "3",
+            "four" => "4",
+            "five" => "5",
+            "six" => "6",
+            "seven" => "7",
+            "eight" => "8",
+            "nine" => "9",
+        }
 
          Map.keys(names_numbers) |> Enum.reduce(string, fn key, str ->
-            String.replace(str, key, names_numbers[key])
+            String.replace(str, key, "#{String.first(key)}#{names_numbers[key]}#{String.last(key)}")
         end)
     end
 
@@ -33,5 +41,3 @@ defmodule Testing do
         numbers |> Enum.reduce(fn num, sum -> num + sum end)
     end
 end
-
-IO.puts(Testing.run_tests()[:example])
