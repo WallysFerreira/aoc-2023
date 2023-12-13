910918eb6ad73e9d3e0c7310b166574bbb6df60a
Wallys Ferreira <wallysfsilva@gmail.com>
2023-12-13 15:29:52 -0300

    get quantity of each cube color

--- day2/part1/lib/part1.ex
+++ day2/part1/lib/part1.ex
@@ -8,11 +8,45 @@ defmodule Part1 do
 
   ## Examples
 
-      iex> Part1.hello()
-      :world
+      iex> Part1.solve_file("example")
+      0
 
   """
-  def hello do
-    :world
+
+  @max_qty %{:red => 12, :green => 13, :blue => 14}
+
+  def read_file(path) do
+    {:ok, contents} = File.read(path)
+    contents |> String.split("\n", trim: true)
+  end
+
+  def parse_to_int(string) do
+    case Integer.parse(string) do
+      {num, ""} -> num
+      :error -> nil
+    end
+  end
+
+  def get_values(line, color) do
+    found = Regex.scan(~r/[\d]+ #{color}/, line)
+
+    values = Enum.reduce(found, [], fn str, list ->
+      [Enum.at(String.split(Enum.at(str, 0)), 0) | list]
+    end)
+
+    Enum.reverse(values)
+  end
+
+  def solve_file(path) do
+    read_file(path) |> Enum.map(fn line ->
+      game = %{
+        :id => String.to_integer(String.slice(line, 5..5)),
+        :blue => get_values(line, "blue"),
+        :red => get_values(line, "red"),
+        :green => get_values(line, "green")
+      }
+    end)
+
+    0
   end
 end
