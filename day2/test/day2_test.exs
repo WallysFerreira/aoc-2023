64d216bf025dc318b33a7035d7b54f67fbbad363
Wallys Ferreira <wallysfsilva@gmail.com>
2023-12-14 21:16:03 -0300

    write test for part 2

--- day2/test/day2_test.exs
+++ day2/test/day2_test.exs
@@ -2,7 +2,11 @@ defmodule Day2Test do
   use ExUnit.Case
   doctest Day2
 
-  test "solves example" do
+  test "solves example of part 1" do
     assert Day2.solve_part_1("files/example") == 8
+  end
+
+  test "solves example of part 2" do
+    assert Day2.solve_part_2("files/example") == 2286
   end
 end
