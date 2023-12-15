dfc90c1158a871409abee78a0463dfa225ff1a65
Wallys Ferreira <wallysfsilva@gmail.com>
2023-12-14 21:58:36 -0300

    initialize project with to-do list

---
+++ day3/lib/day3.ex
@@ -0,0 +1,20 @@
+defmodule Day3 do
+  @moduledoc """
+  Documentation for `Day3`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## To-do list
+  - [ ] Create a list with the symbols
+  - [ ] Get starting and ending coordinates of a number
+  - [ ] Put all characters around the number into a list
+  - [ ] Check if any characters in the list match the symbols and, if so, put the number into a list
+  - [ ] Sum all numbers in the list
+  """
+
+  def hello do
+    :world
+  end
+end
