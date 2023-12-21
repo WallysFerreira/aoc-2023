4038d5b33c28b35cdf06a7d6a748003d46f40ba0
Wallys Ferreira <wallysfsilva@gmail.com>
2023-12-21 20:35:25 -0300

    write test for part 2

--- day3/test/day3_test.exs
+++ day3/test/day3_test.exs
@@ -2,19 +2,27 @@ defmodule Day3Test do
   use ExUnit.Case
   doctest Day3
 
-  test "solves part 1 example file" do
-    assert Day3.solve_part_1("files/example") == 4361
+  describe "part 1" do
+    test "solves example file" do
+      assert Day3.solve_part_1("files/example") == 4361
+    end
+
+    test "solves my example file" do
+      assert Day3.solve_part_1("files/example2") == 0
+    end
+
+    test "solves u/i_have_no_biscuits' example file" do
+      assert Day3.solve_part_1("files/example3") == 413
+    end
+
+    test "solves u/i_have_no_biscuits' second example file" do
+      assert Day3.solve_part_1("files/example4") == 925
+    end
   end
 
-  test "solves my example file" do
-    assert Day3.solve_part_1("files/example2") == 0
-  end
-
-  test "solves u/i_have_no_biscuits' example file" do
-    assert Day3.solve_part_1("files/example3") == 413
-  end
-
-  test "solves u/i_have_no_biscuits' second example file" do
-    assert Day3.solve_part_1("files/example4") == 925
+  describe "part 2" do
+    test "solves example file" do
+      assert Day3.solve_part_2("files/example") == 467835
+    end
   end
 end
