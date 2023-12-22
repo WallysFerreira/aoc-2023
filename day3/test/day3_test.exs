4fbf4f844c751ec4c1c437a9d436d088fa079276
Wallys Ferreira <wallysfsilva@gmail.com>
2023-12-21 21:48:26 -0300

    add more test cases

--- day3/test/day3_test.exs
+++ day3/test/day3_test.exs
@@ -24,5 +24,17 @@ defmodule Day3Test do
     test "solves example file" do
       assert Day3.solve_part_2("files/example") == 467835
     end
+
+    test "solves my example file" do
+      assert Day3.solve_part_2("files/example2") == 0
+    end
+
+    test "solves u/i_have_no_biscuits' example file" do
+      assert Day3.solve_part_2("files/example3") == 6756
+    end
+
+    test "solves u/i_have_no_biscuits' second example file" do
+      assert Day3.solve_part_2("files/example4") == 6756
+    end
   end
 end
