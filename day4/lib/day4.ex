c170ec95ae7966dd8d58a49ceb2993fbd79461ba
Wallys Ferreira <wallysfsilva@gmail.com>
2023-12-21 23:56:07 -0300

    add readme on module

--- day4/lib/day4.ex
+++ day4/lib/day4.ex
@@ -4,15 +4,18 @@ defmodule Day4 do
   """
 
   @doc """
-  Hello world.
 
-  ## Examples
+  # General info
+  The numbers before the "|" character is the list of winning numbers, the numbers after the character is the list of card numbers.
+  Scoring is based on the number of matches between the lists.
+  To calculate the total of points in a card use the formula 2^(x - 1), where x is the number of matches.
+  The answer to the puzzle is the sum of the points in all cards.
 
-      iex> Day4.hello()
-      :world
-
+  ## Part 1 to-do list
+  - [ ] Split winning numbers list and card numbers list
+  - [ ] Count how many numbers appear in both lists
+  - [ ] Use the formula to calculate amount of points in the card
+  - [ ] Sum points in all cards
   """
-  def hello do
-    :world
-  end
+
 end
