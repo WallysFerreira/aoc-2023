37d3133d1d4dc629e84434a51c1f24580f7e1f20
Wallys Ferreira <wallysfsilva@gmail.com>
2023-12-22 00:12:55 -0300

    split winning and card numbers list

--- day4/lib/day4.ex
+++ day4/lib/day4.ex
@@ -12,10 +12,33 @@ defmodule Day4 do
   The answer to the puzzle is the sum of the points in all cards.
 
   ## Part 1 to-do list
-  - [ ] Split winning numbers list and card numbers list
+  - [x] Split winning numbers list and card numbers list
   - [ ] Count how many numbers appear in both lists
   - [ ] Use the formula to calculate amount of points in the card
   - [ ] Sum points in all cards
   """
 
+  def read_file(path) do
+    {:ok, contents} = File.read(path)
+    contents |> String.split("\n", trim: true)
+  end
+
+  def split_lists(line) do
+    clean_line = Regex.replace(~r/Card [\d]+: /, line, "")
+
+    with lists_as_string = clean_line |> String.split(" | ", trim: true) do
+      lists =
+        lists_as_string
+        |> Enum.map(fn number_as_string ->
+          String.split(number_as_string, " ", trim: true)
+          |> Enum.map(&String.to_integer/1)
+        end)
+
+      {Enum.at(lists, 0), Enum.at(lists, 1)}
+    end
+  end
+
+  def solve_part_1(path) do
+    read_file(path) |> Enum.map(&split_lists/1)
+  end
 end
