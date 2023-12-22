417f755c31652512e0fe10f3a9516c6d9e1060a8
Wallys Ferreira <wallysfsilva@gmail.com>
2023-12-22 18:17:07 -0300

    write test for part 2

--- day4/test/day4_test.exs
+++ day4/test/day4_test.exs
@@ -2,9 +2,15 @@ defmodule Day4Test do
   use ExUnit.Case
   doctest Day4
 
-  describe "part" do
+  describe "part 1" do
     test "solves example file" do
       assert Day4.solve_part_1("files/example") == 13
+    end
+  end
+
+  describe "part 2" do
+    test "solves example file" do
+      assert Day4.solve_part_2("files/example") == 30
     end
   end
 end
