223a68f3fd23997db670f2013c82f8210e7bf394
Wallys Ferreira <wallysfsilva@gmail.com>
2023-12-22 20:38:06 -0300

    propagate matches to instances and sum all instances

--- day4/lib/day4.ex
+++ day4/lib/day4.ex
@@ -28,8 +28,8 @@ defmodule Day4 do
 
   ## Part 2 to-do list
   - [x] Add field instances to card object, starting with 1 (the original card)
-  - [ ] Propagate each match on a card object to the adding one instance to the subsequent cards on the list
-  - [ ] Sum instances of all cards
+  - [x] Propagate each match on a card object to the adding one instance to the subsequent cards on the list
+  - [x] Sum instances of all cards
   """
 
   def read_file(path) do
@@ -40,6 +40,8 @@ defmodule Day4 do
   def split_lists(line) do
     clean_line = Regex.replace(~r/Card[\s]+[\d]+: /, line, "")
 
+    card_number = Enum.at(Enum.at(Regex.scan(~r/[\d]+/, line), 0), 0)
+
     with lists_as_string = clean_line |> String.split(" | ", trim: true) do
       lists =
         lists_as_string
@@ -48,14 +50,15 @@ defmodule Day4 do
           |> Enum.map(&String.to_integer/1)
         end)
 
-      {Enum.at(lists, 0), Enum.at(lists, 1)}
+      {Enum.at(lists, 0), Enum.at(lists, 1), String.to_integer(card_number)}
     end
   end
 
   def create_card_object(lists_tuple) do
-    {winning_list, card_list} = lists_tuple
+    {winning_list, card_list, card_number} = lists_tuple
 
     %{
+      id: card_number,
       winning_list: winning_list,
       list: card_list,
       matches: [],
@@ -97,5 +100,63 @@ defmodule Day4 do
       |> Enum.map(&calculate_points/1)
 
     Enum.reduce(cards, 0, fn card_object, points_sum -> points_sum + card_object.points end)
+  end
+
+  def propagate_matches_to_instances(card, cards_list) do
+    card_index = cards_list
+    |> Enum.find_index(fn x -> x == card end)
+
+    indexes_to_propagate =
+      if card.number_of_matches > 0 do
+        Enum.to_list(card_index + 1..card_index + card.number_of_matches)
+        |> Enum.reject(&(&1 >= length(cards_list)))
+      else
+        []
+      end
+
+    indexes_to_propagate
+    |> Enum.reduce(cards_list, fn index_to_propagate, old_list ->
+      card_to_update = Enum.at(old_list, index_to_propagate)
+
+      updated_card = Map.update!(card_to_update, :instances, fn instances ->
+        instances + card.instances
+      end)
+
+      old_list
+      |> List.replace_at(index_to_propagate, updated_card)
+    end)
+  end
+
+  def solve_part_2(path) do
+    lists = read_file(path) |> Enum.map(&split_lists/1)
+
+    cards =
+      lists
+      |> Enum.map(&create_card_object/1)
+
+    cards =
+      cards
+      |> Enum.map(&get_matches/1)
+
+    cards =
+      cards
+      |> Enum.map(&calculate_points/1)
+
+    {updated_cards, _} =
+      cards
+      |> Enum.reduce({cards, 1}, fn card, acc ->
+        {old_list, times_ran} = acc
+
+        card_to_use = if times_ran == 1 do
+          card
+        else
+          Enum.find(old_list, &(&1.id == card.id))
+        end
+
+        {propagate_matches_to_instances(card_to_use, old_list), times_ran + 1}
+      end)
+
+    updated_cards
+    |> Enum.reduce(0, fn card, sum -> sum + card.instances end)
   end
 end
