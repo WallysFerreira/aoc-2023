f41f64c3f7a6bbf440cdf934a7b7092c7e78724d
Wallys Ferreira <wallysfsilva@gmail.com>
2023-12-21 23:59:04 -0300

    write test

--- day4/test/day4_test.exs
+++ day4/test/day4_test.exs
@@ -2,7 +2,9 @@ defmodule Day4Test do
   use ExUnit.Case
   doctest Day4
 
-  test "greets the world" do
-    assert Day4.hello() == :world
+  describe "part" do
+    test "solves example file" do
+      assert Day4.solve_part_1("files/example") == 13
+    end
   end
 end
