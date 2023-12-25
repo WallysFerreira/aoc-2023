b2ca7e75f74caeedbac67ca39dd5c94659de676a
Wallys Ferreira <wallysfsilva@gmail.com>
2023-12-25 21:08:50 +0000

    remove moduledoc

--- day5/lib/day5.ex
+++ day5/lib/day5.ex
@@ -1,17 +1,4 @@
 defmodule Day5 do
-  @moduledoc """
-  Documentation for `Day5`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day5.hello()
-      :world
-
-  """
   def hello do
     :world
   end
