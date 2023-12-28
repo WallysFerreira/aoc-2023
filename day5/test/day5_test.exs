f1513d36f9026b952c41dfea3758086f49b932c5
Wallys Ferreira <wallysfsilva@gmail.com>
2023-12-28 20:27:07 +0000

    write new test

--- day5/test/day5_test.exs
+++ day5/test/day5_test.exs
@@ -3,8 +3,14 @@ defmodule Day5Test do
   doctest Day5
 
   describe "part 1" do
-    test "solves part 1" do
+    test "solves example file" do
       assert Day5.solve_part_1("files/example") == 35
+    end
+  end
+
+  describe "part 2" do
+    test "solves example file" do
+      assert Day5.solve_part_2("files/example") == 46
     end
   end
 end
