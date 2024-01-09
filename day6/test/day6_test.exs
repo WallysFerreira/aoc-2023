3c7e3fc46a0b3175d68cb3f59c9034038a8dec3a
Wallys Ferreira <wallysfsilva@gmail.com>
2024-01-09 00:33:17 +0000

    write main test

--- day6/test/day6_test.exs
+++ day6/test/day6_test.exs
@@ -2,7 +2,9 @@ defmodule Day6Test do
   use ExUnit.Case
   doctest Day6
 
-  test "greets the world" do
-    assert Day6.hello() == :world
+  describe "part 1" do
+    test "solves example file" do
+      assert Day6.solve_part_1("files/example") == 288
+    end
   end
 end
