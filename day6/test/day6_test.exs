c6e3fd298eaf3b4f7d56f75adb028b7702f22b23
Wallys Ferreira <wallysfsilva@gmail.com>
2024-01-09 00:39:06 +0000

    write test

--- day6/test/day6_test.exs
+++ day6/test/day6_test.exs
@@ -6,5 +6,22 @@ defmodule Day6Test do
     test "solves example file" do
       assert Day6.solve_part_1("files/example") == 288
     end
+
+    test "gets races info" do
+      assert Day6.get_races(Day6.read_file("files/example")) == [
+        %{
+          time: 7,
+          distance: 9,
+        },
+        %{
+          time: 15,
+          distance: 40,
+        },
+        %{
+          time: 30,
+          distance: 200,
+        },
+      ]
+    end
   end
 end
