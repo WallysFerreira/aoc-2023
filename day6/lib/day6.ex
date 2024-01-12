42f3464252f77e9658ac0d356bc5e7e74a03afd7
Wallys Ferreira <wallysfsilva@gmail.com>
2024-01-12 19:48:30 +0000

    implement function to extract race info

--- day6/lib/day6.ex
+++ day6/lib/day6.ex
@@ -1,2 +1,37 @@
 defmodule Day6 do
+  def read_file(path) do
+    {:ok, contents} = File.read(path)
+    String.split(contents, "\n", trim: true)
+  end
+
+  def extract_numbers(line, with_index \\ false) do
+    numbers =
+      Regex.scan(~r/[\d]+/, line)
+      |> List.flatten()
+      |> Enum.map(&(String.to_integer(&1)))
+
+    if with_index do
+      Enum.with_index(numbers)
+    else
+      numbers
+    end
+  end
+
+  def get_races(lines) do
+    times = extract_numbers(Enum.at(lines, 0), true)
+    distances = extract_numbers(Enum.at(lines, 1))
+
+    times
+    |> Enum.map(fn time_tuple ->
+      {time, idx} = time_tuple
+
+      %{
+        time: time,
+        distance: Enum.at(distances, idx)
+      }
+    end)
+  end
+
+  def solve_part_1(path) do
+  end
 end
