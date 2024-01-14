c84ee61a9f80c6db58ab351af762c35677b65924
Wallys Ferreira <wallysfsilva@gmail.com>
2024-01-14 18:36:01 +0000

    extract info from file

--- day7/lib/day7_part1.ex
+++ day7/lib/day7_part1.ex
@@ -1,2 +1,17 @@
 defmodule Day7Part1 do
+  def read_file(path) do
+    {:ok, contents} = File.read(path)
+
+    contents
+    |> String.split("\n", trim: true)
+  end
+
+  def get_info(path) do
+    read_file(path)
+    |> Enum.map(&String.split(&1, " ", trim: true))
+    |> Enum.map(&%{
+      hand: String.split(Enum.at(&1, 0), "", trim: :true),
+      bid: String.to_integer(Enum.at(&1, 1))
+    })
+  end
 end
--- day7/test/day7_part1_test.exs
+++ day7/test/day7_part1_test.exs
@@ -1,4 +1,30 @@
 defmodule Day7Part1Test do
   use ExUnit.Case
 
+  @example_path "files/example"
+
+  test "extracts hand and bid info" do
+    assert Day7Part1.get_info(@example_path) == [
+      %{
+        hand: ["3", "2", "T", "3", "K"],
+        bid: 765
+      },
+      %{
+        hand: ["T", "5", "5", "J", "5"],
+        bid: 684
+      },
+      %{
+        hand: ["K", "K", "6", "7", "7"],
+        bid: 28
+      },
+      %{
+        hand: ["K", "T", "J", "J", "T"],
+        bid: 220
+      },
+      %{
+        hand: ["Q", "Q", "Q", "J", "A"],
+        bid: 483
+      }
+    ]
+  end
 end
