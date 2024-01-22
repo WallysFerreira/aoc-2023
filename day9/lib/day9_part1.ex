ea302e8118ef63599e2a4dfc149b17ddb4a19b97
Wallys Ferreira <wallysfsilva@gmail.com>
2024-01-22 20:21:48 +0000

    implement function to read file

--- day9/lib/day9_part1.ex
+++ day9/lib/day9_part1.ex
@@ -1,4 +1,16 @@
 defmodule Day9Part1 do
+  def read_file(path) do
+    {:ok, contents} = File.read(path)
+
+    contents
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      line
+      |> String.split(" ", trim: :true)
+      |> Enum.map(&String.to_integer(&1))
+    end)
+  end
+
   def get_differences(arr) do
     arr
     |> Enum.chunk_every(2, 1, :discard)
--- day9/test/day9_part1_test.exs
+++ day9/test/day9_part1_test.exs
@@ -1,6 +1,12 @@
 defmodule Day9Part1Test do
   use ExUnit.Case
 
+  @example_path "files/example"
+
+  test "reads lists from file" do
+    assert Day9Part1.read_file(@example_path) == [[0, 3, 6, 9, 12, 15], [1, 3, 6, 10, 15, 21], [10, 13, 16, 21, 30, 45]]
+  end
+
   describe "gets sequence of differences" do
     test "with all positives" do
       assert Day9Part1.get_differences([0, 3, 7, 15, 25]) == [3, 4, 8, 10]
