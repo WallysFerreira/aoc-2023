4f019d9517aed9ca6dbc33df2b1d5ef27e3d01f6
Wallys Ferreira <wallysfsilva@gmail.com>
2024-01-22 20:15:23 +0000

    implement function to get difference between numbers in list

--- day9/lib/day9_part1.ex
+++ day9/lib/day9_part1.ex
@@ -1,2 +1,7 @@
 defmodule Day9Part1 do
+  def get_differences(arr) do
+    arr
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(&(Enum.at(&1, 1) - Enum.at(&1, 0)))
+  end
 end
--- day9/test/day9_part1_test.exs
+++ day9/test/day9_part1_test.exs
@@ -1,3 +1,17 @@
 defmodule Day9Part1Test do
   use ExUnit.Case
+
+  describe "gets sequence of differences" do
+    test "with all positives" do
+      assert Day9Part1.get_differences([0, 3, 7, 15, 25]) == [3, 4, 8, 10]
+    end
+
+    test "with all negatives" do
+      assert Day9Part1.get_differences([-40, -30, -27, -15]) == [10, 3, 12]
+    end
+
+    test "with mixed numbers" do
+      assert Day9Part1.get_differences([0, -2, 9, 19, -20]) == [-2, 11, 10, -39]
+    end
+  end
 end
