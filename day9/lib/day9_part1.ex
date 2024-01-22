252113dabaae805f5fd859c3393b8b3b82758e18
Wallys Ferreira <wallysfsilva@gmail.com>
2024-01-22 20:39:16 +0000

    implement function that finds the list of differences until all zeroes

--- day9/lib/day9_part1.ex
+++ day9/lib/day9_part1.ex
@@ -11,9 +11,24 @@ defmodule Day9Part1 do
     end)
   end
 
-  def get_differences(arr) do
-    arr
+  def get_differences(list) do
+    list
     |> Enum.chunk_every(2, 1, :discard)
     |> Enum.map(&(Enum.at(&1, 1) - Enum.at(&1, 0)))
+  end
+
+  def find_all_differences(list) do
+    Stream.cycle(0..1)
+    |> Enum.reduce_while({list, []}, fn _, acc ->
+      {current_list, differences} = acc
+
+      current_dif = get_differences(current_list)
+
+      if Enum.all?(current_dif, fn elem -> elem == 0 end) do
+        {:halt, List.insert_at(differences, -1, current_dif)}
+      else
+        {:cont, {current_dif, List.insert_at(differences, -1, current_dif)}}
+      end
+    end)
   end
 end
--- day9/test/day9_part1_test.exs
+++ day9/test/day9_part1_test.exs
@@ -4,7 +4,11 @@ defmodule Day9Part1Test do
   @example_path "files/example"
 
   test "reads lists from file" do
-    assert Day9Part1.read_file(@example_path) == [[0, 3, 6, 9, 12, 15], [1, 3, 6, 10, 15, 21], [10, 13, 16, 21, 30, 45]]
+    assert Day9Part1.read_file(@example_path) == [
+      [0, 3, 6, 9, 12, 15],
+      [1, 3, 6, 10, 15, 21],
+      [10, 13, 16, 21, 30, 45]
+    ]
   end
 
   describe "gets sequence of differences" do
@@ -18,6 +22,32 @@ defmodule Day9Part1Test do
 
     test "with mixed numbers" do
       assert Day9Part1.get_differences([0, -2, 9, 19, -20]) == [-2, 11, 10, -39]
+    end
+  end
+
+  describe "gets list of differences until all zeroes" do
+    test "in first example" do
+      assert Day9Part1.find_all_differences([0, 3, 6, 9, 12, 15]) == [
+        [3, 3, 3, 3, 3],
+        [0, 0, 0, 0]
+      ]
+    end
+
+    test "in second example" do
+      assert Day9Part1.find_all_differences([1, 3, 6, 10, 15, 21]) == [
+        [2, 3, 4, 5, 6],
+        [1, 1, 1, 1],
+        [0, 0, 0]
+      ]
+    end
+
+    test "in third example" do
+      assert Day9Part1.find_all_differences([10, 13, 16, 21, 30, 45]) == [
+        [3, 3, 5, 9, 15],
+        [0, 2, 4, 6],
+        [2, 2, 2],
+        [0, 0]
+      ]
     end
   end
 end
