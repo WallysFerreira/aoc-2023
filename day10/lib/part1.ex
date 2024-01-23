b2b582be53f81bd9f19a12b6bc854c96017dcd1b
Wallys Ferreira <wallysfsilva@gmail.com>
2024-01-23 21:01:17 +0000

    implement function to read file

--- day10/lib/part1.ex
+++ day10/lib/part1.ex
@@ -1,2 +1,8 @@
 defmodule Part1 do
+  def read_file(path) do
+    {:ok, contents} = File.read(path)
+
+    contents
+    |> String.split("\n", trim: true)
+  end
 end
--- day10/test/part1_test.exs
+++ day10/test/part1_test.exs
@@ -1,2 +1,28 @@
 defmodule Part1Test do
+  use ExUnit.Case
+
+  @example1_path "files/example1"
+  @example2_path "files/example2"
+
+  describe "reads file" do
+    test "from first example" do
+      assert Part1.read_file(@example1_path) == [
+        "-L|F7",
+        "7S-7|",
+        "L|7||",
+        "-L-J|",
+        "L|-JF",
+      ]
+    end
+
+    test "from second example" do
+      assert Part1.read_file(@example2_path) == [
+        "7-F7-",
+        ".FJ|7",
+        "SJLL7",
+        "|F--J",
+        "LJ.LJ",
+      ]
+    end
+  end
 end
