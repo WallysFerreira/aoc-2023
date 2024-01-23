8813e6700de2044c0cc923fa46ddef8d5e829325
Wallys Ferreira <wallysfsilva@gmail.com>
2024-01-23 22:54:05 +0000

    implement function to check if pipes can be connected

--- day10/lib/part1.ex
+++ day10/lib/part1.ex
@@ -5,4 +5,41 @@ defmodule Part1 do
     contents
     |> String.split("\n", trim: true)
   end
+
+  def connectable?(pipe1, pipe2, position_of_pipe1) do
+    connects_with_north = ["|", "L", "J", "S"]
+    connects_with_south = ["|", "7", "F", "S"]
+    connects_with_east = ["-", "L", "F", "S"]
+    connects_with_west = ["-", "7", "J", "S"]
+
+    cond do
+      position_of_pipe1 == :north ->
+        if Enum.member?(connects_with_north, pipe1) do
+          Enum.member?(connects_with_north, pipe2)
+        else
+          false
+        end
+
+      position_of_pipe1 == :south ->
+        if Enum.member?(connects_with_south, pipe1) do
+          Enum.member?(connects_with_south, pipe2)
+        else
+          false
+        end
+
+      position_of_pipe1 == :east ->
+        if Enum.member?(connects_with_east, pipe1) do
+          Enum.member?(connects_with_east, pipe2)
+        else
+          false
+        end
+
+      position_of_pipe1 == :west ->
+        if Enum.member?(connects_with_west, pipe1) do
+          Enum.member?(connects_with_west, pipe2)
+        else
+          false
+        end
+    end
+  end
 end
--- day10/test/part1_test.exs
+++ day10/test/part1_test.exs
@@ -25,4 +25,148 @@ defmodule Part1Test do
       ]
     end
   end
+
+  describe "judges if pipes can connect" do
+    test "horizontal with vertical" do
+      directions = [:west, :east, :north, :south]
+
+      results =
+        directions
+        |> Enum.map(&Part1.connectable?("-", "|", &1))
+
+      assert Enum.any?(results) == false
+    end
+
+    test "horizontal with bendeds" do
+      directions = [:west, :east, :north, :south]
+
+      # Tests with J
+      results_J =
+        directions -- [:west]
+        |> Enum.map(&Part1.connectable?("-", "J", &1))
+
+      assert Enum.any?(results_J) == false
+      assert Part1.connectable?("-", "J", :west) == true
+
+      # Tests with L
+      results_L =
+        directions -- [:east]
+        |> Enum.map(&Part1.connectable?("-", "L", &1))
+
+      assert Enum.any?(results_L) == false
+      assert Part1.connectable?("-", "L", :east) == true
+
+      # Tests with 7
+      results_7 =
+        directions -- [:west]
+        |> Enum.map(&Part1.connectable?("-", "7", &1))
+
+      assert Enum.any?(results_7) == false
+      assert Part1.connectable?("-", "7", :west) == true
+
+      # Tests with F
+      results_F =
+        directions -- [:east]
+        |> Enum.map(&Part1.connectable?("-", "F", &1))
+
+      assert Enum.any?(results_F) == false
+      assert Part1.connectable?("-", "F", :east) == true
+    end
+
+    test "vertical with bendeds" do
+      directions = [:west, :east, :north, :south]
+      bendeds = ["J", "L", "7", "F"]
+
+      results =
+        directions
+        |> Enum.map(fn direction ->
+          bendeds
+          |> Enum.map(&Part1.connectable?("|", &1, direction))
+        end)
+
+      # No vertical should connect from west
+      assert Enum.any?(Enum.at(results, 0)) == false
+
+      # No vertical should connect from east
+      assert Enum.any?(Enum.at(results, 1)) == false
+
+      # Vertical should connect with J and L from north
+      assert Enum.at(Enum.at(results, 2), 0) == true
+      assert Enum.at(Enum.at(results, 2), 1) == true
+      # Not all connecting from north should be true
+      assert Enum.all?(Enum.at(results, 2)) == false
+
+      # Vertical should connect with 7 and F from south
+      assert Enum.at(Enum.at(results, 3), 2) == true
+      assert Enum.at(Enum.at(results, 3), 3) == true
+      # Not all connecting from south should be true
+      assert Enum.all?(Enum.at(results, 3)) == false
+    end
+
+    test "ground with any" do
+      directions = [:west, :east, :north, :south]
+      pipes = ["-", "|", "J", "L", "7", "F", "S"]
+
+      results =
+        directions
+        |> Enum.map(fn direction ->
+          pipes
+          |> Enum.map(&Part1.connectable?(".", &1, direction))
+        end)
+
+      assert Enum.any?(Enum.map(results, &Enum.any?(&1))) == false
+    end
+
+    test "start with any" do
+      directions = [:west, :east, :north, :south]
+      pipes = ["-", "|", "J", "L", "7", "F"]
+
+      results =
+        directions
+        |> Enum.map(fn direction ->
+          pipes
+          |> Enum.map(&Part1.connectable?("S", &1, direction))
+        end)
+
+      # From west
+      # -
+      assert Enum.at(Enum.at(results, 0), 0) == true
+      # J
+      assert Enum.at(Enum.at(results, 0), 2) == true
+      # 7
+      assert Enum.at(Enum.at(results, 0), 4) == true
+      # Others
+      assert Enum.all?(Enum.at(results, 0)) == false
+
+      # From east
+      # -
+      assert Enum.at(Enum.at(results, 1), 0) == true
+      # L
+      assert Enum.at(Enum.at(results, 1), 3) == true
+      # F
+      assert Enum.at(Enum.at(results, 1), 5) == true
+      # Others
+      assert Enum.all?(Enum.at(results, 1)) == false
+
+      # From north
+      # |
+      assert Enum.at(Enum.at(results, 2), 1) == true
+      # J
+      assert Enum.at(Enum.at(results, 2), 2) == true
+      # L
+      assert Enum.at(Enum.at(results, 2), 3) == true
+      # Others
+      assert Enum.all?(Enum.at(results, 2)) == false
+
+      # From south
+      # |
+      assert Enum.at(Enum.at(results, 3), 1) == true
+      # 7
+      assert Enum.at(Enum.at(results, 3), 4) == true
+      # F
+      assert Enum.at(Enum.at(results, 3), 5) == true
+      # Others
+      assert Enum.all?(Enum.at(results, 3)) == false
+    end
+  end
 end
