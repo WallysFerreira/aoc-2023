c6204d872c316b88bb5d7435ee798a6fba45c21e
Wallys Ferreira <wallysfsilva@gmail.com>
2024-02-16 19:51:52 -0300

    add files and implement read_file

--- day11/test/day11_part1_test.exs
+++ day11/test/day11_part1_test.exs
@@ -1,3 +1,12 @@
 defmodule Day11Part1Test do
   use ExUnit.Case
+
+  @example_path "files/example"
+
+  test "returns a list of string when reading file" do
+    lines = Day11Part1.read_file(@example_path)
+
+    assert is_list(lines)
+    assert Enum.all?(lines, &is_binary(&1))
+  end
 end
