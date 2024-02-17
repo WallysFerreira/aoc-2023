f3029a3945344b174769268c0b4299ec48cf6a2b
Wallys Ferreira <wallysfsilva@gmail.com>
2024-02-16 22:05:22 -0300

    implement get_columns

--- day11/lib/day11_part1.ex
+++ day11/lib/day11_part1.ex
@@ -5,4 +5,35 @@ defmodule Day11Part1 do
     contents
     |> String.split("\n", trim: true)
   end
+
+  def remove_first_column(map) do
+    map
+    |> Enum.map(&String.slice(&1, 1..-1//1))
+  end
+
+  def get_columns(map) do
+    cols_qnt = String.length(Enum.at(map, 0))
+
+    {cols, _} =
+      Enum.to_list(0..(cols_qnt - 1))
+      |> Enum.reduce({[], map}, fn _, acc ->
+        {cols, updated_map} = acc
+
+        updated_cols =
+          cols
+          |> List.insert_at(
+            -1,
+            updated_map
+            |> Enum.map(&String.first(&1))
+          )
+
+        {updated_cols, remove_first_column(updated_map)}
+      end)
+
+    cols
+  end
+
+  def expand(path) do
+    read_file(path)
+  end
 end
--- day11/test/day11_part1_test.exs
+++ day11/test/day11_part1_test.exs
@@ -2,6 +2,7 @@ defmodule Day11Part1Test do
   use ExUnit.Case
 
   @example_path "files/example"
+  # @example_expanded_path "files/example_expanded"
 
   test "returns a list of string when reading file" do
     lines = Day11Part1.read_file(@example_path)
@@ -9,4 +10,19 @@ defmodule Day11Part1Test do
     assert is_list(lines)
     assert Enum.all?(lines, &is_binary(&1))
   end
+
+  test "correctly extracts columns" do
+    map = Day11Part1.read_file(@example_path)
+    columns = Day11Part1.get_columns(map)
+
+    assert is_list(columns)
+    assert Enum.at(columns, 0) == [".", ".", "#", ".", ".", ".", ".", ".", ".", "#"]
+    assert Enum.at(columns, 2) == [".", ".", ".", ".", ".", ".", ".", ".", ".", "."]
+    assert Enum.at(columns, 5) == [".", ".", ".", ".", ".", ".", ".", ".", ".", "."]
+    assert Enum.at(columns, 8) == [".", ".", ".", ".", ".", ".", ".", ".", ".", "."]
+  end
+
+  # test "expands space" do
+  #  assert Day11Part1.expand(@example_path) == Day11Part1.read_file(@example_expanded_path)
+  # end
 end
